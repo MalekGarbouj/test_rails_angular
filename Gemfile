5270cd8be3f9fa433f2665df307893d0aa64a56e
MalekGarbouj <malekgarbouj40@gmail.com>
2020-01-13 15:26:46 +0100

    first commit

--- Gemfile
+++ Gemfile
@@ -17,6 +17,10 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'mini_racer', platforms: :ruby
 gem 'bower-rails'
 
+gem 'responders'
+
+gem 'angular_rails_csrf'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :events
   root 'dashboard#index'
+
+  namespace :api do
+    resources :events, only: [:index, :create] do
+      collection do
+        get :search
+      end
+    end
+  end
 end
